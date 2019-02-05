81e286347696ac6b2b5db8d43b7a6d3ef424612a
HansRanda <rsa6@naver.com>
2019-02-06 01:32:43 +0900

    last

--- App.js
+++ App.js
@@ -1,90 +1,58 @@
 import React from 'react';
-import { View, StyleSheet, Button, Alert, Platform } from 'react-native';
-import { Constants, Notifications, Permissions } from 'expo';
+import { View, StyleSheet, Button, Alert, Platform, SafeAreaView } from 'react-native';
+import { MAIN_CUSTOM_COLOR } from './constants';
+// import { Constants, Notifications, Permissions } from 'expo';
 import DropdownAlert from 'react-native-dropdownalert';
+import List from './List';
 
 class App extends React.Component {
-
-  componentWillMount() {
-    this.getiOSNotificationPermission();
-    this.listenForNotifications();
+  constructor(props) {
+    super(props);
   }
 
-  componentDidMount() {
-    this.fetchData();
-  }
-
-  async getiOSNotificationPermission() {
-    const { status } = await Permissions.getAsync(
-      Permissions.NOTIFICATIONS
-    ); // check permission
-
-    if (status !== 'granted') {
-      await Permissions.askAsync(Permissions.NOTIFICATIONS);
+  onSelect({ item, index }) {
+    switch (item.type) {
+      case 'close':
+        this.forceClose();
+        break;
+      default:
+        const random = Math.floor(Math.random() * 1000 + 1);
+        const title = item.type + ' #' + random;
+        this.dropdown.alertWithType(item.type, title, item.message);
     }
   }
 
-  fetchData = async () => {
-    try {
-      await fetch('https://mywebsite.com/endpoint/');
-    } catch (error) {
-      this.dropdown.alertWithType('error', 'Error', error.message);
-    }
-  };
-  
-  listenForNotifications = () => {
-    Notifications.addListener(notification => {
-      if (notification.origin === 'received' && Platform.OS === 'ios') {
-        Alert.alert(this.notificationData().title, this.notificationData().body);
-      } // in app alert
-    });
-  };
-
-  handleSubmit = () => {
-    let sendAfterFiveSeconds = Date.now();
-    sendAfterFiveSeconds += 3000;
-    const schedulingOptions = { time: sendAfterFiveSeconds };
-
-    Notifications.scheduleLocalNotificationAsync(
-      this.notificationData(),
-      schedulingOptions
-    );
-  };
-
-  notificationData() {
-    const localnotification = {
-      title: '로컬 알림 테스트',
-      body: '이것은 로컬알림 테스트 입니다... push 를 받고 싶으시면 앱을 끄고 받아주세요.',
-      android: {
-        sound: true,
-      },
-      ios: {
-        sound: true,
-      },
-    };
-    return localnotification;
+  forceClose() {
+    this.dropdown.close();
   }
-
+  onClose(data) {
+    console.log(data);
+  }
+  onCancel(data) {
+    console.log(data);
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Button
-          title="Please wait 3 Seconds"
-          onPress={this.handleSubmit}
+      <SafeAreaView style={styles.container}>
+      
+        <List onSelect={({ item, index }) => this.onSelect({ item, index })} />
+
+        <DropdownAlert
+          ref={ref => this.dropdown = ref}
+          containerStyle={{backgroundColor: MAIN_CUSTOM_COLOR}}
+          showCancel={true}
+          onClose={data => this.onClose(data)}
+          onCancel={data => this.onCancel(data)}
         />
-        <DropdownAlert ref={ref => this.dropdown = ref} />
-      </View>
+      </SafeAreaView>
     );
   }
-
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'white',
   },
 });
 
